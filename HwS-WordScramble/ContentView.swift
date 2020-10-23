64fee6ce0c1ba34477ee1a5706d763ac630433d2
Brian Foster <brianisbrilliant@gmail.com>
2020-10-22 21:41:07 -0500

    Initial Commit

---
+++ HwS-WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HwS-WordScramble
+//
+//  Created by Brian Foster on 10/22/20.
+//  Copyright © 2020 Brian Foster. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
